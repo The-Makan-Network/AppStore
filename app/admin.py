74c4a006bb8f08013c9435ee9a9314ff672bc592
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-03-30 18:17:29 +0800

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 # Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('username', 'phoneno', 'password1')
