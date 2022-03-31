b106ad7a5c78b942bf999b8f6e305ee8db8c8bdb
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-03-31 18:07:36 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,101 @@
 from django.shortcuts import render, redirect
 from django.db import connection
 
+def home(request):
+    """Shows the product listing page"""
+
+    ## Use raw query to get all objects
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM products ORDER BY productid")
+        products = cursor.fetchall()
+
+    result_dict = {'products': products}
+
+    return render(request, 'app/home.html', result_dict)
+
+
+
+# Create your views here.
+def register(request):
+    """Shows the main page"""
+    context = {}
+    status = ''
+
+    if request.POST:
+        form = NewUserForm(request.POST)
+        ## Check if userid is already in the table
+        with connection.cursor() as cursor:
+
+            cursor.execute("SELECT * FROM allusers WHERE userid = %s", [request.POST['username']])
+            user = cursor.fetchone()
+            ## No customer with same id
+            if user == None:
+                ##TODO: date validation
+                cursor.execute("INSERT INTO allusers(userid, phoneno, password) VALUES (%s, %s, %s)"
+                        , [request.POST['username'], request.POST['phoneno'], request.POST['password1'] ])
+                newuser = form.save()
+                login(request, newuser)
+                messages.success(request, 'Registration successful.')
+                return redirect('/')    
+            else:
+                status = 'User with ID %s already exists' % (request.POST['username'])
+
+    form = NewUserForm()
+    context['status'] = status
+ 
+    return render(request, "app/register.html", context)
+
+def signin(request):
+    """Shows the login page"""
+    context = {}
+    status = ''
+
+    if request.POST:
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password1=password1)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'You are now logged in as {username}.')
+                return redirect('app/')
+            else:
+                messages.error(request,'Invalid username or password.')
+        else:
+            status = 'Invalid username or password.' 
+    form = AuthenticationForm()
+    context['status'] = status
+    return render(request, 'app/login.html', context)
+			
+		
+def profile(request, id):
+    """Shows the main page"""
+
+    ## Use raw query to get all objects
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM allusers WHERE userid =  %s",[id])
+        user = cursor.fetchone()
+
+    result_dict = {'users': users}
+
+    return render(request, 'app/profile.html', result_dict)
+
+
+def view(request, id):
+    """Shows the main page"""
+    
+    ## Use raw query to get a customer
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM products WHERE productid = %s", [id])
+        customer = cursor.fetchone()
+    result_dict = {'cust': customer}
+
+    return render(request,'app/view.html',result_dict)
+
+
+
+"""
 # Create your views here.
 def index(request):
     """Shows the main page"""
@@ -112,38 +207,38 @@ def login(request):
                 status = 'Your User Id and Password is incorrect' % (request.POST['customerid'])
 
     return render(request, 'app/login.html', context)
-
+"""
 
 # Create your views here.
-def index_products(request):
+#def index_products(request):
     """Shows the product listing page"""
 
     ## Use raw query to get all objects
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM products ORDER BY productid")
-        products = cursor.fetchall()
+#    with connection.cursor() as cursor:
+#        cursor.execute("SELECT * FROM products ORDER BY productid")
+#        products = cursor.fetchall()
 
-    result_dict = {'products': products}
+#    result_dict = {'products': products}
 
-    return render(request, 'app/index_products.html', result_dict)
+#    return render(request, 'app/index_products.html', result_dict)
 
-def purchase(request, productid):
-    """Shows the main page"""
+#def purchase(request, productid):
+#    """Shows the main page"""
 
     ## Use raw query to get all objects
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM products WHERE productid =  %s",[productid])
-        products = cursor.fetchone()
+#    with connection.cursor() as cursor:
+#        cursor.execute("SELECT * FROM products WHERE productid =  %s",[productid])
+#        products = cursor.fetchone()
 
-    result_dict = {'products': products}
+#    result_dict = {'products': products}
 
-    return render(request, 'app/purchase.html', result_dict)
+#    return render(request, 'app/purchase.html', result_dict)
 
-def profile(request, phoneno):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM allusers WHERE phoneno =  %s",[phoneno])
-        products = cursor.fetchone()
+#def profile(request, phoneno):
+#    with connection.cursor() as cursor:
+#        cursor.execute("SELECT * FROM allusers WHERE phoneno =  %s",[phoneno])
+#        products = cursor.fetchone()
 
-    result_dict = {'allusers': allusers}
+#    result_dict = {'allusers': allusers}
 
-    return render(request, 'app/profile.html', result_dict)
+#    return render(request, 'app/profile.html', result_dict)
