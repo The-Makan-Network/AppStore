2807f81ca997d9389b0063ed8791ddd1ec6d0017
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-04-02 03:09:10 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,9 @@
+from django.db import models
 
+class Loginteste(models.Model):
+    username = models.CharField(max_length=50, blank=True, null=True)
+    password = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'loginteste'
