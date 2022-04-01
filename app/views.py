e6a909002a3181a888bd335c4f1e9225966bd705
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-04-01 12:34:16 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -43,6 +43,29 @@ def register(request):
     form = NewUserForm()
     return render(request, "app/register.html", {})
 
+
+def signin(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, ("You are now logged in as {username}))
+                return redirect('register')
+            else:
+                messages.error(request, ("Invalid username or password."))
+        else:
+            messages.success(request, ("Invalid username or password."))
+    form = AuthenticationForm()
+    return render(request,
+                    "app/login.html",
+                    context={"form":form})
+
+
+"""
 def signin(request):
 
     if request.POST:
@@ -57,6 +80,8 @@ def signin(request):
              return redirect('login')
 
     return render(request, 'app/login.html', {})
+    
+"""
 		
 def profile(request, id):
     """Shows the main page"""
