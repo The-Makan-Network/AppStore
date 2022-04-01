8c34bd301b2c5d0fd982da063f82937c2359da58
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-04-02 02:15:58 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import NewUserForm
+from .models import MyUser
 
 # Create your views here.
 def home(request):
@@ -18,6 +19,58 @@ def home(request):
     return render(request, 'app/home.html', result_dict)
 
 
+# Create your views here.
+def register(request):
+
+    if request.POST:
+        form = MyUser(request.POST)
+        ## Check if userid is already in the table
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM allusers WHERE userid = %s", [request.POST['username']])
+            user = cursor.fetchone()
+            ## No customer with same id
+            if not form.is_valid():
+                messages.success(request, ("Password does not pass requirements. Please try again."))
+                return redirect('register')
+            elif user == None:
+                ##TODO: date validation
+                cursor.execute("INSERT INTO allusers(userid, phoneno, password) VALUES (%s, %s, %s)"
+                        , [request.POST['username'], request.POST['phoneno'], request.POST['password1'] ])
+                newuser = form.save()
+                login(request, newuser)
+                messages.success(request, ("Registration successful. Welcome, {username}!"))
+                return redirect('login')
+            else:
+                messages.success(request, ("Username or Phone Number already taken. Please Try Again."))
+                return redirect('register')
+		
+    form = MyUser()
+    return render(request, "app/register.html", {})
+
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password1']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'Welcome, You logged in to {user.username}')
+            return redirect('home')
+        else:
+            messages.success(request, ("There Was An Error Logging In, Try Again."))	
+            return redirect('login')	
+
+
+    else:
+        return render(request, 'app/login.html', {})
+
+def signout(request):
+	logout(request)
+	messages.success(request, ("You Were Logged Out!"))
+	return redirect('home')
+
+"""
 # Create your views here.
 def register(request):
 
@@ -68,7 +121,7 @@ def signout(request):
 	logout(request)
 	messages.success(request, ("You Were Logged Out!"))
 	return redirect('home')
-
+"""
 		
 def profile(request, id):
     """Shows the main page"""
