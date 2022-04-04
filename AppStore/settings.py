60c3dc222735f32b3f1c8a2c9a65b4fca22bc658
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-04-04 22:54:13 +0800

    Update settings.py

--- AppStore/settings.py
+++ AppStore/settings.py
@@ -109,11 +109,6 @@ else:
         }
     }
 
-    
-AUTHENTICATION_BACKENDS = [
-    'app.auth_backends.MyAuthBackend',
-    # 'django.contrib.auth.backends.ModelBackend',
-]    
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
