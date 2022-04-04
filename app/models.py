7b369805475f2e6a69a05fa34483867402456595
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-04-04 22:52:05 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,1 @@
-from django.db import models
 
-class Loginteste(models.Model):
-    userid = models.CharField(max_length=50, blank=True, null=True)
-    password = models.CharField(max_length=50, blank=True, null=True)
-    phoneno = models.IntegerField(max_length=50, blank=True, null=True)
-
-    class Meta:
-        managed = True
-        db_table = 'allusers'
