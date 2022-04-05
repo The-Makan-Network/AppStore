10996883326e659aaff3614dca603e559f7f2ae8
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-04-06 02:58:01 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-class useraccounts(models.Model):
-  username = models.TextField(null=True)
-  phoneno = models.IntegerField(null=True)
-  password = models.TextField(null=True)
+class Allusers(models.Model):
+  username = models.CharField(unique=True, max_length=16)
+  phoneno = models.BigIntegerField(primary_key=True)
+  password = models.CharField(max_length=12)
+  
+  class Meta:
+    managed = False
+    db_table = 'allusers'
