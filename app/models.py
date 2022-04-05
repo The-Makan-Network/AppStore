75d2774fa25fe612fbd8eaeae4c1fc9ce238d2aa
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-04-05 14:37:37 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,6 @@
+from django.db import models
 
+class useraccounts(models.Model):
+  username = models.TextField(null=True)
+  phoneno = models.IntegerField(null=True)
+  password = models.TextField(null=True)
