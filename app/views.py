d2d8d35c9e48319798207c757bb509ab5cedfb17
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-04-06 19:42:06 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -120,9 +120,8 @@ def signin(request):
                     #user = NewUserForm(created)
                     #login_user = created.save()
                     login(request, created)
-                    username = User.username
-                    messages.success(request, f'Welcome, You logged in to {account[1]}')
-                    return redirect('profile', {'users':username})
+                    messages.success(request, f'Welcome, You logged in to {username}')
+                    return redirect('profile', {username})
                 else:
                     messages.success(request, f'Invalid, You logged in to password {account[2]}')
             messages.success(request, f'{username} {password}')	
