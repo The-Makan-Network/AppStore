474192571d4910c14b5de092d3f8123c834e0979
wanfeijong <37286614+wanfeijong@users.noreply.github.com>
2022-04-06 23:04:03 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -194,7 +194,7 @@ def purchase(request):
     with connection.cursor() as cursor:
         cursor.execute("INSERT INTO transactions(b_id, s_id, p_id, qty, delivery, status) VALUES (%s, %s, %s, %s, %s, %s)"
                 , [request.POST['b_id'], request.POST['s_id'], request.POST['p_id'], request.POST['qty'], request.POST['delivery'], "pending"])
-        return render(request, 'app/thanku.html', {})
+        return render(request, 'app/home.html', {})
                         , 	
 
 """
